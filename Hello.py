753f252c72d206ab2d9e119cb1ebf5d29b7835ca
Baabie <pamella.nomura@gmail.com>
2021-09-09 21:12:34 -0300

    s nei oq aconteceu

---
+++ Hello.py
@@ -0,0 +1,16 @@
+print("Bem vindo ao jogo da Adivinhação")
+
+numero_secreto = 42
+
+chute_str = input("Digite o seu numero: ")
+
+print("Você digitou " , chute_str) 
+
+chute = int(chute_str)
+
+if(numero_secreto == chute): 
+    print("Você acertou")
+else: 
+    print("Você errou")
+
+print("Fim do jogo")
