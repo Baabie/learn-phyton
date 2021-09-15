f710abfc58d613a4dacad9dc75d32b90ff355414
Baabie <pamella.nomura@gmail.com>
2021-09-14 22:42:44 -0300

    adicionado notas

--- jogo.py
+++ jogo.py
@@ -13,7 +13,7 @@ for rodada in range(1, total_de_tentativas + 1):
     if(chute < 1 or chute > 100):
         print('Você deve digitar um número entre 1 e 100!')
         continue 
-
+        #continue para continuar com a próxima iteralção
     acertou = chute == numero_secreto
     menor = chute < numero_secreto
     maior = chute > numero_secreto
@@ -23,10 +23,12 @@ for rodada in range(1, total_de_tentativas + 1):
     if(acertou): 
         print("Você acertou")
         break
+    #break encerra o ciclo
     elif(maior):
         print("Você errou! O seu chute foi maior do que o número secreto.")
     elif(menor):
         print("Você errou! O seu chute foi menor do que o número secreto.")
 
-  
+  #com o for não precisa colocar rodada = rodada + 1  for já faz isso
     print("Fim de jogo")
+#iteração repetição de um conjunto de instruções por quantidade finita de vezes ou enquanto uma condição seja aceita
